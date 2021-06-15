95af1f2779825331f9b5ae8ccc3df09cdc4839df
Paweł <49080906+shaaqu@users.noreply.github.com>
2021-06-16 00:27:56 +0200

    add tree

--- expert.pl
+++ expert.pl
@@ -1,8 +1,55 @@
 # DRZEWO DECYZYJNE
 
-tree(if_then_else('expensive?',
-                if_then_else()))
+tree(if_then_else('Czy za granica?',
+                # za granica
+                if_then_else('Czy w Europie?',
+                    # w europie
+                    if_then_else('Czy nad morzem?',
+                        # nad morzem
+                        miejsce(Lazurowe_Wybrzeze),
+                        # nie nad mozem
+                        if_then_else('Czy aktywnie?',
+                            # aktywnie
+                            miejsce(Sycylia),
+                            # nie aktywnie
+                            miejsce(Barcelona)
+                        )
+                    )
+                    # nie w europie
+                    if_then_else('Czy w gorach?',
+                        # w gorach
+                        miejsce(Nepal),
+                        # nie w gorach
+                        miejsce(Wietnam)
+                    )
+                )
+                # nie za granica
+                if_then_else('Czy tanio?',
+                    # tanio
+                    if_then_else('Czy zwiedzac?',
+                        # zwiedzac
+                        miejsce(Torun),
+                        # nie zwiedzac
+                        miejsce(Sopot)
+                    )
+                    # nie tanio
+                    if_then_else('Czy SPA?',
+                        # spa
+                        miejsce(Krakow),
+                        # nie spa
+                        miejsce(Warszawa)
+                    )
+                )
+    )
+)
 
 # BAZA WIEDZY
+
+miejsce(A) :-
+        tree(T),
+        tree_miejsce(T, A).
+
+
+miejsce(Lazurowe_Wybrzeze) :- is_true('')
 
 # INTERFEJS UZYTKOWNIKA
